42ffb83aafada867e60dac34c1b63b83324aea65
Dima <starchenkodmitrypavlovich@gmail.com>
2023-09-22 20:02:44 +0300

    refactoring

--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -11,25 +11,19 @@ export class LoginDto {
 
 export class SignUpDto {
 
-    // @IsString()
-    // @IsNotEmpty()
     @MinLength(4)
     login:string;
     
-    // @IsString()
     @MinLength(4)
     @IsEmail()
     email:string;
 
-    // @IsString()
     @MinLength(2)
     password:string;
 
-    
     @IsString()
     name:string;
 
     @IsString()
     surname:string;
-
 }
