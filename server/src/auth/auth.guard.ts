ace1cd419ce3efe4fe84c32be1df08f22e0ca6b9
Dima <starchenkodmitrypavlovich@gmail.com>
2023-09-23 02:35:58 +0300

    added NotAuthGuard

--- server/src/auth/auth.guard.ts
+++ server/src/auth/auth.guard.ts
@@ -10,14 +10,30 @@ export class AuthGuard implements CanActivate {
     try{
       const request:Request = context.switchToHttp().getRequest();
 
-      const token = request.signedCookies[REQ_KET_TOKEN];
+      const token = request.cookies[REQ_KET_TOKEN];
       console.log(`${REQ_KET_TOKEN}: `,token);
 
       return token !== undefined;
     }catch(error){
       console.log("Error: ",error);
+      return false;
     }
-    finally{
+  }
+}
+
+
+@Injectable()
+export class NotAuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean{
+    try{
+      const request:Request = context.switchToHttp().getRequest();
+
+      const token = request.cookies[REQ_KET_TOKEN];
+      console.log(`${REQ_KET_TOKEN}: `,token);
+
+      return token === undefined;
+    }catch(error){
+      console.log("Error: ",error);
       return false;
     }
   }
