a881df69230dac35c203f79d354b77ac0d8f4a12
Dima <starchenkodmitrypavlovich@gmail.com>
2023-09-23 23:51:38 +0300

    added api

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,15 @@
+import api from "@/api/api";
 import styles from "./page.module.scss"
 import MainScreen from "@/components/screens/MainScreen";
 
 
 
-export default function Main() {  
+export default async function Main() {  
+  console.log("Main page")
+
+  const res =  await api.get("page/main")
+  console.log("API: ",res.data)
+
   return (<MainScreen/>);
 }
 
