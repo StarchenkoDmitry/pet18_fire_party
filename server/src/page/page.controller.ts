87e4899e79c8b7688579d9581daf42371e075e98
Dima <starchenkodmitrypavlovich@gmail.com>
2023-09-23 23:31:23 +0300

    added page resource

---
+++ server/src/page/page.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PageService } from './page.service';
+import { CreatePageDto } from './dto/create-page.dto';
+import { UpdatePageDto } from './dto/update-page.dto';
+
+@Controller('page')
+export class PageController {
+  constructor(private readonly pageService: PageService) {}
+
+  @Get()
+  findAll() {
+    return this.pageService.findAll();
+  }
+}
