52de06e565aedcec83f2079fe852074a9723e2ac
Dima <starchenkodmitrypavlovich@gmail.com>
2023-10-12 23:30:00 +0300

    AvatarEditorModal is ready

--- client/src/components/profile/avatarEditor/AvatarEditorModal.tsx
+++ client/src/components/profile/avatarEditor/AvatarEditorModal.tsx
@@ -1,9 +1,12 @@
 import styles from "./AvatarEditorModal.module.scss";
 
-import { ConvertCanvasToDataURL } from "@/utils/Convert";
+import { ConvertBlobToStringBase64, ConvertCanvasToDataURL } from "@/utils/Convert";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
-import AvatarEditor, { ImageState } from "react-avatar-editor";
+import AvatarEditor from "react-avatar-editor";
 import AvatarPreview from "./ui/AvatarPreview";
+
+
+const TIME_OF_UPDATE = 150;//ms
 
 
 export interface AvatarEditorModalProps{
@@ -16,14 +19,42 @@ export default function AvatarEditorModal({doClose,loadImage,saveImage}:AvatarEd
     console.log("Rendering AvatarEditorModal")
 
     const editor = useRef<AvatarEditor>(null);
+
     const [scale,setScale] = useState(1);
+    const [rotation,setRotation] = useState(0);
     
     const [image,setImage] = useState<string>("");
     const [imagePreview,setImagePreview] = useState<string>("");
 
-    const inputFile = useRef(null);
+    const inputFile = useRef<HTMLInputElement>(null);
     
-    useEffect(()=>{
+
+    const resetOption = ()=>{
+        setScale(1);
+        setRotation(0);
+    }
+
+    const openFinderFile = ()=>{
+        if(inputFile.current)
+        inputFile.current.click();
+    }
+    
+    const loadFileToImage = async (event: ChangeEvent<HTMLInputElement>)=>{
+        // console.log("event ",event)
+        if(!event.target.files || event.target.files.length === 0)return;
+
+        const file:File | null = event.target.files.item(0);
+        if(!file)return;
+
+        if(saveImage){            
+            await saveImage(await ConvertBlobToStringBase64(file));
+            // startUpdatePreviewImage();
+            StartLoadImage();
+            resetOption();
+        }
+    }
+
+    const StartLoadImage = async ()=>{
         if(!loadImage) return;
         loadImage().then(res=>{
             if(res){
@@ -32,13 +63,13 @@ export default function AvatarEditorModal({doClose,loadImage,saveImage}:AvatarEd
                 setImage("/img/user.png");
             }
         });
-    },[]);
-
-    const changeScale = (event: ChangeEvent<HTMLInputElement>) => {
-        const scale = parseFloat(event.target.value)
-        setScale( scale )
     }
 
+    useEffect(()=>{
+        StartLoadImage();
+    },[]);
+
+    
     const doSaveImage = async () => {
         if (editor && editor.current) {
             const canvas = editor.current.getImage()
@@ -49,74 +80,54 @@ export default function AvatarEditorModal({doClose,loadImage,saveImage}:AvatarEd
 
             if(!saveImage)return;
             await saveImage(await ConvertCanvasToDataURL(canvas));
+            await StartLoadImage();
+            resetOption();
         }
     }
 
-    const timeOfUpdate = 100;//ms
-
-    const refka = useRef<{
-        canavsImage:HTMLCanvasElement | undefined,
-        funcdo: NodeJS.Timeout  | undefined
-    }>({
-        canavsImage:undefined,
-        funcdo: undefined
-    });
-
-    const doingImg = async ()=>{
-        const currentImg  =refka.current.canavsImage;
-        refka.current.canavsImage = undefined;
-        console.log("addImageOnDoinger ",currentImg)
-
-        if(currentImg){
-            setImagePreview(await ConvertCanvasToDataURL(currentImg));
+    const refka = useRef<NodeJS.Timeout  | undefined>();    
+    const updatePreviewImage = async ()=>{
+        if (!editor || !editor.current){
+            refka.current = undefined;
+            return;
         }
+
+        const img = editor.current.getImage()
+        if(!img){
+            refka.current = undefined;
+            return;
+        }
+
+        setImagePreview(await ConvertCanvasToDataURL(img));
         
-        if(refka.current.canavsImage){
-            refka.current.funcdo = setTimeout(doingImg,timeOfUpdate);
-        }else{
-            refka.current.funcdo = undefined;
+        refka.current = undefined;
+    }
+    const startUpdatePreviewImage = ()=>{
+        if(!refka.current){
+            refka.current = setTimeout(()=>{
+                updatePreviewImage();
+            },TIME_OF_UPDATE);
         }
     }
 
-    const addImageOnDoinger= (img: HTMLCanvasElement)=>{
-        // console.log("addImageOnDoinger")
-        refka.current.canavsImage = img;
-
-        if(!refka.current.funcdo){
-            refka.current.funcdo = setTimeout(doingImg,timeOfUpdate);
-        }
+    const rotationEvent = (event: ChangeEvent<HTMLInputElement>)=>{
+        setRotation( parseFloat(event.target.value) );
+        startUpdatePreviewImage();
     }
-
 
     const mouseMoveEvent = async (event: Event)=>{
-        // console.log("EVENT")
-        if (!editor || !editor.current)return;
-        const img = editor.current.getImage()
-        if(!img) return;
-
-        addImageOnDoinger(img);
+        startUpdatePreviewImage();
     }
     
-
-
-    const OnImageReady = async (event: Event)=>{
-        console.log("OnImageReady event: ",event)
-
-        if (editor && editor.current) {
-            console.log("editor.current: ",editor.current)
-            const canvas = editor.current.getImage()
-
-            if(!canvas) return;
-
-            console.log("canvas ",canvas)
-
-            if(!saveImage)return;
-            const dataULR = await ConvertCanvasToDataURL(canvas);
-
-            setImagePreview(dataULR);
-        }
+    const scaleEvent = (event: ChangeEvent<HTMLInputElement>) => {
+        setScale( parseFloat(event.target.value) );
+        startUpdatePreviewImage();
     }
-
+    
+    const imageReadyEvent = async (event: Event)=>{
+        startUpdatePreviewImage();
+    }
+    
     return (
         <div className={styles.modal} onClick={doClose}>
             <div className={styles.content} onClick={(e)=>e.stopPropagation()}>
@@ -135,26 +146,39 @@ export default function AvatarEditorModal({doClose,loadImage,saveImage}:AvatarEd
                         color={[0,0,0,.75]}
                         scale={scale}
 
+                        rotate={rotation}
+
                         onMouseMove={mouseMoveEvent}
-                        onImageReady={OnImageReady}
-                    /><br/>
-                    <div className="editor">
-                        <input ref={inputFile} type="file" id="file" style={{display:"none"}} />
-                        <button onClick={()=>{
+                        onImageReady={imageReadyEvent}
+                    />
+                    <div className={styles.editorSettings}>
 
-                        }}>Upload a picture</button>
+                        <input onChange={loadFileToImage} ref={inputFile} type="file" id="file" style={{display:"none"}} />
+                        <button onClick={openFinderFile} className={styles.btn_file}>Add a picture file</button>
 
-                        <div>Zoom:{scale}</div>
+                        <div>Zoom ({scale})</div>
                         <input
                             name="scale"
                             type="range"
-                            onChange={changeScale}
+                            onChange={scaleEvent}
                             min='0.1'
-                            max="2"
+                            max="2.5"
                             step="0.01"
-                            defaultValue="1"
+                            value={scale}
                         />
-                        <button onClick={doSaveImage}>Save</button>
+                        
+                        <div>Rotation ({rotation})</div>
+                        <input
+                            name="rotation"
+                            type="range"
+                            onChange={rotationEvent}
+                            min="-180"
+                            max="180"
+                            step="1"
+                            value={rotation}
+                        />
+
+                        <button className={styles.btn_save} onClick={doSaveImage}>Save</button>
                     </div>
                 </div>
                 <h3 className={styles.editorPreview_lable}>Preview</h3>
@@ -165,34 +189,3 @@ export default function AvatarEditorModal({doClose,loadImage,saveImage}:AvatarEd
         </div>
     )
 }
-
-
-
-
-    // const mouseMoveEvent = async (event: Event)=>{
-    //     console.log("EVENT")
-    //     if (!editor || !editor.current)return;
-    //     const img = editor.current.getImage()
-    //     if(!img) return;
-
-    //     setImagePreview(await ConvertCanvasToDataURL(img));
-    // }
-
-
-
-
-    // const doSaveImage = async () => {
-    //     if (editor && editor.current) {
-    //         const canvas = editor.current.getImage()//размер как у Editor 
-    //         const canvasScaled = editor.current.getImageScaledToCanvas()//роазмер с маштабом
-
-    //         if(!canvas || !canvasScaled) return;
-
-    //         console.log("canvas,canvasScaled",canvas,canvasScaled)
-            
-    //         console.log("canvas: ",await ConvertCanvasToBlob(canvas))
-    //         console.log("canvasScaled: ",await ConvertCanvasToBlob(canvasScaled))
-
-    //         await UpdateImage("2",await ConvertCanvasToBlob(canvas))
-    //     }
-    // }
