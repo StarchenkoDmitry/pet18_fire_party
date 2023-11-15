f561b4d20689ebee58d65a9979f34d0d07fb5711
Dima <starchenkodmitrypavlovich@gmail.com>
2023-11-15 17:39:12 +0300

    improved AddChatModal

--- client/src/components/communication/modals/AddChatModal.tsx
+++ client/src/components/communication/modals/AddChatModal.tsx
@@ -2,30 +2,28 @@ import styles from "./AddChatModal.module.scss";
 
 import { useEffect, useState } from "react";
 
-import { IUser, IUserForSearch } from "@/common/user.interface";
-import { FindAllByName } from "@/actions/User.actions";
+import { UserView } from "../ui/search/UserView";
+
 import { CreateChat } from "@/actions/Chat.actions";
+
 import { useUsersSearch } from "@/store/UsersSearch";
 import { useConnect } from "@/store/Connent";
 
 
-
-export interface Props{
+export interface AddChatModalProps{
     doClose?:()=>void
 }
 
-export default function AddChatModal({doClose}:Props) {
-    const [text,setText] = useState("")
-    // const [users,setUsers] = useState<IUser[] | undefined>()
-
+export default function AddChatModal({doClose}:AddChatModalProps) {
     const { subConnect, unsubConnect} = useConnect.getState()
+
+    const [text,setText] = useState("")
 
     const sercher = useUsersSearch.getState()
     const users = useUsersSearch(state=>state.users)
 
     useEffect(()=>{
         subConnect(sercher)
-
         return ()=>unsubConnect(sercher)
     },[])
 
@@ -33,8 +31,8 @@ export default function AddChatModal({doClose}:Props) {
         sercher.search(text)
     },[text])
 
-    const addChat = (id:string)=>{
-        CreateChat(id)
+    const createChat = (userId:string)=>{
+        CreateChat(userId)
     }
 
     const changeText = (event: React.ChangeEvent<HTMLInputElement>)=>{
@@ -42,19 +40,27 @@ export default function AddChatModal({doClose}:Props) {
     }
 
     return (
-        <div className={styles.modal +" "+ styles.active} onClick={doClose}>
+        <div className={styles.modal} onClick={doClose}>
             <div className={styles.modal_content} onClick={(e)=>e.stopPropagation()}>
-                <div className={styles.blockSearch}>
+                <div className={styles.searchBlock}>
                     <span className={styles.label}>Search by name</span>
-                    <input className={styles.input} type="text" value={text}
-                    onChange={changeText}/>
+                    <input 
+                        className={styles.input}
+                        placeholder="name..."
+                        type="text" 
+                        value={text}
+                        onChange={changeText}
+                    />
                 </div>
-                {
-                    users?.map((u,i)=><div key={i}>
-                        <span>{u.name}</span>
-                        <button onClick={()=>addChat(u.id)} >add</button>
-                    </div>)
-                }
+                <div className={styles.usersList + " scrollbar1"}>
+                    {
+                        [...users]?.map((u,i)=><UserView
+                            key={i} 
+                            user={u} 
+                            createChat={createChat} 
+                        />)
+                    }
+                </div>
             </div>
         </div>
     )
