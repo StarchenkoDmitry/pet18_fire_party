9d5edd69a0193c2ca99772e38251ade301e75c90
Dima <starchenkodmitrypavlovich@gmail.com>
2023-11-27 17:55:25 +0300

    refactor SelectChat

--- client/src/components/communication/ui/SelectChat.tsx
+++ client/src/components/communication/ui/SelectChat.tsx
@@ -1,9 +1,10 @@
 import styles from "./SelectChat.module.scss";
 
+
 export default function SelectChat() {
     return (
         <div className={styles.selectChat}>
-            <span>Please select a chat</span>
+            <span className={styles.text}>Start communicating, select chat</span>
         </div>
-    );
+    )
 }
