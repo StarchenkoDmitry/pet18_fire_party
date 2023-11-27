d351d3dfad0fccfe2a9c0c6828a13815cb19b9f6
Dima <starchenkodmitrypavlovich@gmail.com>
2023-11-27 18:51:25 +0300

    added func GetBaseIPSocket

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -2,18 +2,24 @@ import axios from "axios";
 
 
 //http://127.0.0.1:3000/api
+export function GetBaseIP(){
+    const base = window.location.hostname
+    return `http://${base}:3000/api`
+}
 
-
-function GetBaseIP(){
-    let base = window.location.hostname;
-    return `http://${base}:3000/api`;
+//http://127.0.0.1:3020
+export function GetBaseIPSocket(){
+    const base = window.location.hostname
+    return `http://${base}:3020`
 }
 
 
-const baseURL = GetBaseIP();
+
+const baseURL = GetBaseIP()
 
 const api = axios.create({
     baseURL: baseURL,
     withCredentials:true
-});
-export default api;
+})
+
+export default api
