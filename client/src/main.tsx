832d3a351c693bb00d7170e27e3badf07fbf4089
Dima <starchenkodmitrypavlovich@gmail.com>
2023-12-02 03:45:32 +0300

    refacrtor main file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,7 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-// import './index.css'
-// import { BrowserRouter } from 'react-router-dom'
+
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(
 //   //<React.StrictMode>
@@ -10,8 +9,4 @@ import App from './App.tsx'
 //   //</React.StrictMode>,
 // )
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
-)
-
-
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
