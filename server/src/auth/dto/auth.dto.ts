5543f9d8974d47cf288e0213ed73b6e7ea5ef9f5
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-22 17:50:48 +0300

    add new constants

--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -1,27 +1,37 @@
 import { IsEmail, Length, MaxLength } from "class-validator";
+import {
+    MAX_EMAIL_LENGTH,
+    MAX_LOGIN_LENGTH,
+    MAX_NAME_LENGTH,
+    MAX_PASSWORD_LENGTH,
+    MAX_SURNAME_LENGTH,
+    MIN_EMAIL_LENGTH,
+    MIN_LOGIN_LENGTH,
+    MIN_PASSWORD_LENGTH,
+} from "src/common/constants";
 
 export class LoginDto {
-    @Length(4, 16)
+    @Length(MIN_LOGIN_LENGTH, MAX_LOGIN_LENGTH)
     login: string;
 
-    @Length(4, 32)
+    @Length(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
     password: string;
 }
 
 export class SignUpDto {
-    @Length(4, 16)
+    @Length(MIN_LOGIN_LENGTH, MAX_LOGIN_LENGTH)
     login: string;
 
-    @Length(4, 32)
+    @Length(MIN_EMAIL_LENGTH, MAX_EMAIL_LENGTH)
     @IsEmail()
     email: string;
 
-    @Length(4, 32)
+    @Length(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH)
     password: string;
 
-    @MaxLength(16)
+    @MaxLength(MAX_NAME_LENGTH)
     name: string;
 
-    @MaxLength(16)
+    @MaxLength(MAX_SURNAME_LENGTH)
     surname: string;
 }
--- server/src/common/constants.ts
+++ server/src/common/constants.ts
@@ -1,5 +1,15 @@
+//how many messages will be loaded
+export const MAX_CHAT_COUNT = 126;
+
+
+//register
 export const MAX_NAME_LENGTH = 14;
 export const MAX_SURNAME_LENGTH = 14;
 
-//how many messages will be loaded
-export const MAX_CHAT_COUNT = 126;
+export const MAX_EMAIL_LENGTH = 32;
+export const MIN_EMAIL_LENGTH = 4;
+
+export const MAX_LOGIN_LENGTH = 16;
+export const MIN_LOGIN_LENGTH = 4;
+export const MAX_PASSWORD_LENGTH = 16;
+export const MIN_PASSWORD_LENGTH = 4;
