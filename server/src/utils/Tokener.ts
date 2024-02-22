40c87c9bd2c410912a1a565adff54f6ce6815c8a
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-22 21:04:06 +0300

    delete Tokener file

--- server/src/utils/Tokener.ts
+++
@@ -1,8 +0,0 @@
-export function CreateToken(): string {
-    const max = 20;
-    let str = "";
-    for (let i = 0; i < max; i++) {
-        str += (Math.random() * 9).toFixed(0);
-    }
-    return str;
-}
