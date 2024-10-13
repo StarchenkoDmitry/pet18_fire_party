caa4b55bb8a7998840141e15f481d76c641ed04f
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-10-13 17:49:08 +0300

    ref(client): vite.config.js

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
-    host:"127.0.0.1",
-    https: {
-      key: './ssl/privatekey.key',
-      cert: './ssl/certificate.crt',
-    },
   },
   resolve: {
     alias: [
