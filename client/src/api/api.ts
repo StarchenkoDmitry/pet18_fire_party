f51e6f355de4180a29507f133d63d17089763781
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-10-13 17:44:43 +0300

    ref(client): api.ts

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -2,14 +2,16 @@ import axios from "axios";
 
 //http://127.0.0.1:3000/api
 export function GetBaseIP() {
-    const base = window.location.hostname;
-    return `https://${base}:3000/api`;
+    const hostname = window.location.hostname;
+    // return `https://${base}:3000/api`;
+    return `${window.location.protocol}//${hostname}:3000/api`;
 }
 
 //wss://127.0.0.1:3000
 export function GetBaseIPSocket() {
-    const base = window.location.hostname;
-    return `wss://${base}:3000`;
+    const hostname = window.location.hostname;
+    const protocol = window.location.protocol === "http:" ? "ws:":"wss:";
+    return `${protocol}//${hostname}:3000`;
 }
 
 const api = axios.create({
